ad444f2193378ccbd58d376a57b19cef53d43575
Illyoung Choi <iychoi@email.arizona.edu>
2023-04-04 19:01:08 -0700

    Remove unused user struct

--- volume-service/types/device.go
+++ volume-service/types/device.go
@@ -6,18 +6,12 @@ const (
 	deviceIDPrefix string = "dev_"
 )
 
-// TODO: Need to separate user information out of Device
-type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
 // Device represents a device, holding all necessary info. about device
 type Device struct {
 	ID       string `json:"id"`
 	IP       string `json:"ip"`
 	Username string `json:"username"`
-	Password string `json:"password"` // TODO: need to remove this, use User struct
+	Password string `json:"password"`
 	Storage  string `json:"storage"`
 	//Description string `json:"description,omitempty"`
 }
