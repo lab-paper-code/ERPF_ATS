0e5a44ae4f7ccdbdd1acdf16d43c1b5d3f4aa70a
jeun-990806 <kjeun990806@gmail.com>
2023-04-18 16:36:29 +0900

    그래프 생성 시, 명령줄 인수로 Input, output file path 입력받게 변경

--- io_tracing/get_chart.py
+++ io_tracing/get_chart.py
@@ -1,13 +1,18 @@
+import sys
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
+csv_file_path = sys.argv[1]
+output_file_path = sys.argv[2]
+
 # 데이터 로드
-df = pd.read_csv('strace_timeline.csv')
+df = pd.read_csv(csv_file_path)
 
 df = df[df['File Path'].str.endswith('.JPEG')]
 
 # 시간(Time)을 가로축으로, 파일 경로(File Path)를 세로축으로 설정
-plt.figure(figsize=(df['Time'].nunique()*0.8,
+plt.figure(figsize=(40,
            df['File Path'].nunique()*0.3))  # 차트 크기 설정
 plt.xlabel('Time (s)')  # x축 레이블 설정
 plt.ylabel('File Path')  # y축 레이블 설정
@@ -18,11 +23,8 @@ colors = {'openat': 'blue', 'lseek': 'red', 'read': 'green'}
 # 파일 경로(File Path), Syscall 종류(Syscall), Offset를 이용하여 점 찍기
 for index, row in df.iterrows():
     # 점의 크기 조정
-    if row['Offset'] == 0:
-        dot_size = 100
-    else:
-        dot_size = row['Offset'] / 1000  # Offset 값에 따라 크기 조정
+    dot_size = 100
     plt.scatter(row['Time'], row['File Path'],
                 color=colors[row['Syscall']], s=dot_size)
 
-plt.savefig('all_chart.png')
+plt.savefig(output_file_path)
