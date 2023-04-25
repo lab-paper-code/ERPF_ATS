df641478d539c820a35dec12f634465d7b8d4db6
jeun-990806 <kjeun990806@gmail.com>
2023-04-25 16:03:59 +0900

    image_training.py 인수 변경 (이미지 개수 고정, 데이터셋 경로 추가)

--- io_tracing/image_training.py
+++ io_tracing/image_training.py
@@ -7,12 +7,26 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-if len(sys.argv) != 4:
-  print('Usage: python3 image_training.py <number_of_images> <batch_size> <epochs>')
+import time
 
-n = int(sys.argv[1])
+class time_history(tf.keras.callbacks.Callback):
+   def __init__(self):
+      super().__init__()
+
+   def on_train_begin(self, logs):
+      self.start = time.time()
+   
+   def on_train_end(self, logs):
+      print('Total Training time: ', time.time() - self.start)
+
+if len(sys.argv) != 4:
+  print('Usage: python3 image_training.py <dataset_path> <batch_size> <epochs>')
+
+dataset_path = sys.argv[1]
 batch_size = int(sys.argv[2])
 epochs = int(sys.argv[3])
+
+start = time.time()
 
 # Load MobileNet model without top layers
 base_model = MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
@@ -34,16 +48,19 @@ model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metric
 # Load and preprocess the data
 train_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
-        './images/validation_images',
+        dataset_path,
         target_size=(224, 224),
         batch_size=batch_size,
         class_mode='categorical')
 
+time_callback = time_history()
+
 # Fine-tune the model
 model.fit(
         train_generator,
-        steps_per_epoch=n//batch_size, # number of images in the dataset divided by the batch size
-        epochs=epochs)
+        steps_per_epoch=2000//batch_size, # number of images in the dataset divided by the batch size
+        epochs=epochs,
+        callbacks=[time_callback])
 
-# Save the model
-model.save('mobile_net_finetuned.h5')
+end = time.time()
+print('Total execution time: ', end-start)
