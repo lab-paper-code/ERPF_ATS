57d7d264b2b3d79e10a639e1c6922eb4ba574598
jeun-990806 <kjeun990806@gmail.com>
2023-05-03 10:32:55 +0900

    fine-tuning시 dataset shuffle을 외부에서 수행하게 변경

--- io_tracing/image_training.py
+++ io_tracing/image_training.py
@@ -4,29 +4,23 @@ import tensorflow as tf
 from tensorflow.keras.applications.mobilenet import MobileNet
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
+import tensorflow.keras.preprocessing as pp 
 
-import time
-
-class time_history(tf.keras.callbacks.Callback):
-   def __init__(self):
-      super().__init__()
-
-   def on_train_begin(self, logs):
-      self.start = time.time()
-   
-   def on_train_end(self, logs):
-      print('Total Training time: ', time.time() - self.start)
+def export_access_pattern(current_epoch, epochs, paths_list):
+   with open('access_pattern_'+str(current_epoch), 'w') as f:
+      for paths in paths_list:
+         in_epoch = paths.numpy()
+         for path in in_epoch:
+            f.write(path.decode('utf-8') + '\n')
 
 if len(sys.argv) != 4:
   print('Usage: python3 image_training.py <dataset_path> <batch_size> <epochs>')
+  exit(1)
 
 dataset_path = sys.argv[1]
 batch_size = int(sys.argv[2])
 epochs = int(sys.argv[3])
-
-start = time.time()
 
 # Load MobileNet model without top layers
 base_model = MobileNet(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
@@ -43,24 +37,32 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # Compile the model
-model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=SGD(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Load and preprocess the data
-train_datagen = ImageDataGenerator(rescale=1./255)
-train_generator = train_datagen.flow_from_directory(
+train_datagen = pp.image.ImageDataGenerator(rescale=1./255)
+dataset = pp.image_dataset_from_directory(
         dataset_path,
-        target_size=(224, 224),
+        image_size=(224, 224),
         batch_size=batch_size,
-        class_mode='categorical')
+        labels='inferred',
+        label_mode='categorical',
+        shuffle=False)
 
-time_callback = time_history()
+# dataset을 셔플하면 file_paths 속성이 사라지므로, ZipDataSet으로 변경
+file_paths = tf.data.Dataset.from_tensor_slices((dataset.file_paths)).batch(batch_size)
+dataset_with_path = tf.data.Dataset.zip((dataset, file_paths))
 
 # Fine-tune the model
-model.fit(
-        train_generator,
-        steps_per_epoch=2000//batch_size, # number of images in the dataset divided by the batch size
-        epochs=epochs,
-        callbacks=[time_callback])
-
-end = time.time()
-print('Total execution time: ', end-start)
+for i in range(epochs):
+   if shuffle:
+      dataset_with_path = dataset_with_path.shuffle(10000)
+   print('Epoch ' + str(i+1) + '/' + str(epochs))
+   image_dataset = dataset_with_path.map(lambda img, path: img).prefetch(tf.data.AUTOTUNE)
+   path_dataset = tf.data.Dataset.from_tensor_slices(dataset.file_paths).batch(batch_size)
+   export_access_pattern(i+1, epochs, path_dataset) # Access 순서
+   model.fit(
+         image_dataset,
+         steps_per_epoch=10000//batch_size,
+         epochs=1,
+         shuffle=False)
