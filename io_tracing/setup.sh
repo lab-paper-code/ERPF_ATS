d3bf6a51f004c6ac602264b997a2fa5237a80b21
jeun-990806 <kjeun990806@gmail.com>
2023-05-03 10:32:11 +0900

    setup.sh 명령줄 인수 수정

--- io_tracing/setup.sh
+++ io_tracing/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-dataset_path=$2
+dataset_path=$1
 
 # Download ImageNet Dataset
 wget $dataset_path
@@ -14,15 +14,15 @@ rm ILSVRC2012_img_val.tar
 # 이미지를 카테고리 별로 분류
 sh image_categorize.sh
 
-# 카테고리 별 이미지 파일을 2개씩만 남김 (총 2,000장이 되게끔)
+# 카테고리 별 이미지 파일을 10개씩만 남김 (총 10,000장이 되게끔)
 for dir in */
 do
   cd "$dir"
   jpeg_files=$(ls *.JPEG 2>/dev/null)
 
-  if [ $(echo "$jpeg_files" | wc -l) -gt 2 ]
+  if [ $(echo "$jpeg_files" | wc -l) -gt 10 ]
   then
-    excess=$(echo "$jpeg_files" | head -n -2)
+    excess=$(echo "$jpeg_files" | head -n -10)
     rm $excess
   fi
   cd ..
