e4a76d3485e4521c136937dff31fe7c943647559
Illyoung Choi <iychoi@email.arizona.edu>
2023-05-30 16:23:29 -0700

    Implement ingress for webdav

--- volume-service/k8s/old/ingress.go
+++ volume-service/k8s/old/ingress.go
@@ -38,23 +38,12 @@ spec:
                   number: 80
 
 
-// getWebdavSvcName makes webdavIngress name
-func (client *K8sClient) getWebdavIngressName(volumeID string) string {
-	return fmt.Sprintf("%s%s", volumeID, ingWebdavSuffix)
-}
 
 // getAppSvcName makes appIngress name
 func (client *K8sClient) getAppIngressName(volumeID string) string {
 	return fmt.Sprintf("%s%s", volumeID, ingAppSuffix)
 }
 
-func (client *K8sClient) getIngressNamespace() string {
-	return ingNamespace
-}
-
-func (client *K8sClient) getWebdavIngressPath(volumeID string) string {
-	return fmt.Sprintf("%s%s", ingWebdavPathSuffix, volumeID)
-}
 
 func (client *K8sClient) getAppIngressPath(volumeID string) string {
 	return fmt.Sprintf("%s%s", ingAppPathSuffix, volumeID)
@@ -62,121 +51,6 @@ func (client *K8sClient) getAppIngressPath(volumeID string) string {
 
 
 
-
-// CreateIngress Webdav creates a pvc for the given volumeID
-func (client *K8sClient) CreateWebdavIngress(username string, volumeID string) error {
-	logger := log.WithFields(log.Fields{
-		"package":  "k8s",
-		"struct":   "K8sClient",
-		"function": "CreateWebdavIngress",
-	})
-
-	logger.Debugf("Creating a Webdav Ingress for user %s, volume id %s", username, volumeID)
-
-	pathPrefix := networkingv1.PathTypePrefix
-
-	claim := &networkingv1.Ingress{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: client.getWebdavIngressName(volumeID),
-			Namespace: client.getIngressNamespace(),
-			Annotations: map[string]string{
-				"kubernetes.io/ingress.class": "nginx",
-				"nginx.ingress.kubernetes.io/proxy-connect-timeout": "150",
-    			"nginx.ingress.kubernetes.io/proxy-read-timeout": "150",
-
-			},
-		},
-		Spec: networkingv1.IngressSpec{
-			Rules: []networkingv1.IngressRule{
-				{
-					IngressRuleValue: networkingv1.IngressRuleValue{
-						HTTP: &networkingv1.HTTPIngressRuleValue{
-							Paths: []networkingv1.HTTPIngressPath{
-								{
-									Path: client.getWebdavIngressPath(volumeID),
-									PathType: &pathPrefix,
-									Backend: networkingv1.IngressBackend{
-										Service: &networkingv1.IngressServiceBackend{
-											Name: client.getWebdavSVCName(volumeID),
-											Port: networkingv1.ServiceBackendPort{
-												Number: 80,
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-
-			},
-		},
-	}
-	//beta
-	// claim := &v1beta1.Ingress{
-	// 	ObjectMeta: metav1.ObjectMetat{
-	// 		Name: client.getWebdavIngressName(volumeID),
-	// 		Namespace: client.getIngressNamespace(),
-	// 		Annotations: map[string]string{
-	// 			"kubernetes.io/ingress.class": "nginx",
-	// 			"nginx.ingress.kubernetes.io/proxy-connect-timeout": "150",
-    // 			"nginx.ingress.kubernetes.io/proxy-read-timeout": "150",
-
-	// 		},
-	// 	},
-	// 	Spec: v1beta1.InressSpec{
-	// 		Rules: []v1beta1.IngressRule{
-	// 			{
-	// 				IgressRuleValue: v1beta1.IngressRuleValue{
-	// 					HTTP: &v1beta1.HTTPIngressRuleValue{
-	// 						Paths: []v1beta1.HTTPIngressPath{
-	// 							{Path: client.getWebdavIngressPath(volumeID)},
-	// 						},
-	// 						Backend: v1beta1.IngressBackend{
-	// 							ServiceName: client.getWebdavSVCName(volumeID),
-	// 							ServicePort: map[string]interface{},
-	// 						},
-	// 					},
-	// 				},
-	// 			},
-
-	// 		},
-	// 	},
-	// }
-
-	webdavIngclient := client.clientSet.NetworkingV1().Ingresses(client.getVolumeNamespace())
-
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(context.Background(), k8sTimeout)
-	defer cancel()
-
-	_, err := webdavIngclient.Get(ctx, claim.GetName(), metav1.GetOptions{})
-
-	if err != nil {
-		// failed to get an existing claim
-		_, err = webdavIngclient.Create(ctx, claim, metav1.CreateOptions{})
-		if err != nil {
-			print(err,"\n")
-			// failed to create one
-			log.Fatal(err)
-			logger.Errorf("Failed to create a Webdav ingress for user %s, volume id %s", username, volumeID)
-			return err
-		}
-
-		logger.Debugf("Created a Webdav ingress for user %s, volume id %s", username, volumeID)
-	} else {
-		_, err = webdavIngclient.Update(ctx, claim, metav1.UpdateOptions{})
-		if err != nil {
-			// failed to create one
-			logger.Errorf("Failed to update a Webdav ingress for user %s, volume id %s", username, volumeID)
-			return err
-		}
-
-		logger.Debugf("Updated a Webdav ingress for user %s, volume id %s", username, volumeID)
-	}
-
-	return nil
-}
 
 
 func (client *K8sClient) CreateAppIngress(username string, volumeID string) error {
--- volume-service/k8s/webdav.go
+++ volume-service/k8s/webdav.go
@@ -8,6 +8,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
@@ -17,6 +18,8 @@ const (
 	webdavDeploymentNamespace  string = objectNamespace
 	webdavServiceNamePrefix    string = "webdav"
 	webdavServiceNamespace     string = objectNamespace
+	webdavIngressNamePrefix    string = "webdav"
+	webdavIngressNamespace     string = objectNamespace
 
 	webdavContainerVolumeName  string = "webdav-storage"
 	webdavContainerPVMountPath string = "/uploads"
@@ -26,8 +29,12 @@ func (adapter *K8SAdapter) GetWebdavDeploymentName(volumdID string) string {
 	return fmt.Sprintf("%s_%s", webdavDeploymentNamePrefix, volumdID)
 }
 
-func (adapter *K8SAdapter) GetWebdavServiceName(volumdID string) string {
-	return fmt.Sprintf("%s_%s", webdavServiceNamePrefix, volumdID)
+func (adapter *K8SAdapter) GetWebdavServiceName(volumeID string) string {
+	return fmt.Sprintf("%s_%s", webdavServiceNamePrefix, volumeID)
+}
+
+func (adapter *K8SAdapter) GetWebdavIngressName(volumeID string) string {
+	return fmt.Sprintf("%s_%s", webdavIngressNamePrefix, volumeID)
 }
 
 func (adapter *K8SAdapter) getWebdavDeploymentLabels(volume *types.Volume) map[string]string {
@@ -44,6 +51,18 @@ func (adapter *K8SAdapter) getWebdavServiceLabels(volume *types.Volume) map[stri
 	labels["volume-id"] = volume.ID
 	labels["device-id"] = volume.DeviceID
 	return labels
+}
+
+func (adapter *K8SAdapter) getWebdavIngressLabels(volume *types.Volume) map[string]string {
+	labels := map[string]string{}
+	labels["webdav-name"] = adapter.GetWebdavIngressName(volume.ID)
+	labels["volume-id"] = volume.ID
+	labels["device-id"] = volume.DeviceID
+	return labels
+}
+
+func (adapter *K8SAdapter) GetWebdavIngressPath(volumeID string) string {
+	return fmt.Sprintf("/%s", volumeID)
 }
 
 func (adapter *K8SAdapter) getWebdavContainers(device *types.Device, volume *types.Volume) []apiv1.Container {
@@ -288,6 +307,102 @@ func (adapter *K8SAdapter) deleteWebdavService(volumeID string) error {
 	return nil
 }
 
+func (adapter *K8SAdapter) createWebdavIngress(volume *types.Volume) error {
+	logger := log.WithFields(log.Fields{
+		"package":  "k8s",
+		"struct":   "K8SAdapter",
+		"function": "createWebdavIngress",
+	})
+
+	logger.Debug("received createWebdavIngress()")
+
+	webdavIngressName := adapter.GetWebdavIngressName(volume.ID)
+	webdavIngressLabels := adapter.getWebdavIngressLabels(volume)
+
+	pathPrefix := networkingv1.PathTypePrefix
+
+	ingress := &networkingv1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      webdavIngressName,
+			Labels:    webdavIngressLabels,
+			Namespace: webdavIngressNamespace,
+			Annotations: map[string]string{
+				"kubernetes.io/ingress.class":                       "nginx",
+				"nginx.ingress.kubernetes.io/proxy-connect-timeout": "150",
+				"nginx.ingress.kubernetes.io/proxy-read-timeout":    "150",
+			},
+		},
+		Spec: networkingv1.IngressSpec{
+			Rules: []networkingv1.IngressRule{
+				{
+					IngressRuleValue: networkingv1.IngressRuleValue{
+						HTTP: &networkingv1.HTTPIngressRuleValue{
+							Paths: []networkingv1.HTTPIngressPath{
+								{
+									Path:     adapter.GetWebdavIngressPath(volume.ID),
+									PathType: &pathPrefix,
+									Backend: networkingv1.IngressBackend{
+										Service: &networkingv1.IngressServiceBackend{
+											Name: adapter.GetWebdavServiceName(volume.ID),
+											Port: networkingv1.ServiceBackendPort{
+												Number: 80,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), operationTimeout)
+	defer cancel()
+
+	ingressClient := adapter.clientSet.NetworkingV1().Ingresses(webdavIngressNamespace)
+	_, err := ingressClient.Get(ctx, ingress.GetName(), metav1.GetOptions{})
+	if err != nil {
+		// does not exist
+		_, createErr := ingressClient.Create(ctx, ingress, metav1.CreateOptions{})
+		if createErr != nil {
+			return createErr
+		}
+	} else {
+		// exist -> update
+		_, updateErr := ingressClient.Update(ctx, ingress, metav1.UpdateOptions{})
+		if updateErr != nil {
+			return updateErr
+		}
+	}
+
+	return nil
+}
+
+func (adapter *K8SAdapter) deleteWebdavIngress(volumeID string) error {
+	logger := log.WithFields(log.Fields{
+		"package":  "k8s",
+		"struct":   "K8SAdapter",
+		"function": "deleteWebdavIngress",
+	})
+
+	logger.Debug("received deleteWebdavIngress()")
+
+	webdavIngressName := adapter.GetWebdavIngressName(volumeID)
+
+	ctx, cancel := context.WithTimeout(context.Background(), operationTimeout)
+	defer cancel()
+
+	ingressClient := adapter.clientSet.NetworkingV1().Ingresses(webdavIngressNamespace)
+	err := ingressClient.Delete(ctx, webdavIngressName, *metav1.NewDeleteOptions(0))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (adapter *K8SAdapter) CreateWebdav(device *types.Device, volume *types.Volume) error {
 	logger := log.WithFields(log.Fields{
 		"package":  "k8s",
@@ -307,13 +422,11 @@ func (adapter *K8SAdapter) CreateWebdav(device *types.Device, volume *types.Volu
 		return err
 	}
 
-	/*
-		//make Webdav ingress
-		err = k8sClient.createWebdavIngress(input.Username, volumeID)
-		if err != nil {
-			panic(err)
-		}
-	*/
+	err = adapter.createWebdavIngress(volume)
+	if err != nil {
+		panic(err)
+	}
+
 	return nil
 }
 
@@ -326,14 +439,17 @@ func (adapter *K8SAdapter) DeleteWebdav(volumeID string) error {
 
 	logger.Debug("received DeleteWebdav()")
 
-	//TODO: Add deleteWebdavIngress
-
 	err := adapter.deleteWebdavService(volumeID)
 	if err != nil {
 		return err
 	}
 
 	err = adapter.deleteWebdavDeployment(volumeID)
+	if err != nil {
+		return err
+	}
+
+	err = adapter.deleteWebdavIngress(volumeID)
 	if err != nil {
 		return err
 	}
