09010690d19dffacf851bd4c330155483ec2b26b
Jiseob-Byeon <alfred.j.byeon@gmail.com>
2023-07-14 12:05:34 +0900

    device_app_registration.py

--- volume-service/test/edge/device_app_registration.py
+++ volume-service/test/edge/device_app_registration.py
@@ -6,7 +6,13 @@ output:
 '''
 import requests
 
-def app_register_request(Serv_url, Name,RequireGPU, DockerImage, Description, Arguments, OpenPorts): 
+def getLoginInfo(): # function getting ID, PASSWORD for sending request
+    print("앱 등록을 위해서는 ID, PASSWORD가 필요합니다.")
+    id=input("ID: ")
+    passwd=input("PASSWORD: ")
+    return id, passwd
+
+def AppRegisterRequest(Serv_url, Name,RequireGPU, DockerImage, Description, Arguments, OpenPorts, id, passwd): # function requesting registration
     data = {
         'Name': Name,
         'RequireGPU': RequireGPU,
@@ -15,26 +21,27 @@ def app_register_request(Serv_url, Name,RequireGPU, DockerImage, Description, Ar
         'Arguments': Arguments,
         'OpenPorts': OpenPorts
     }
-    response = requests.post(Serv_url, data=data) # post request
+    response = requests.post(Serv_url, data=data, auth=(id,passwd), verify=False) # post request, added auth
     return response
 
-Serverurl = '155.230.36.25:31200/apps' # set serverURL, change this to var.
-print("앱 등록을 위한 정보를 받습니다.")
+ID, PASSWORD=getLoginInfo() # get LoginInfo
 
+''' basic settings for registration'''
+print("앱 등록을 위한 정보를 받습니다.")
+Serverurl = '155.230.36.25:31200/apps' # set serverURL, change this to var.
 RequireGPU=False
 Description=""
 Arguments=""
-OpenPorts="12300"
+OpenPorts=[12300]
 Name=input("앱 이름을 입력하세요: ")
-RequireGPU=input("GPU 요청여부를 입력하세요(사용:True, 사용하지 않음:False). 입력하지 않으면 False으로 처리됩니다.:  ")
-Description=input("Description을 입력하세요 입력하지 않으면 공백으로 처리됩니다.: ")
+RequireGPU=input("GPU 요청여부를 입력하세요(사용:True, 사용하지 않음:False).\n입력하지 않으면 False으로 처리됩니다:  ")
+Description=input("Description을 입력하세요.\n입력하지 않으면 공백으로 처리됩니다: ")
 DockerImage=input("사용할 도커 이미지를 입력하세요: ")
-Arguments=input("실행에 필요한 인자(Arguments)를 입력하세요. 입력하지 않으면 인자를 받지 않는 것으로 처리됩니다.: ")
-OpenPorts=input("포트를 입력하세요. 입력하지 않으면 기본포트(12300)으로 처리됩니다. : ")
-response = app_register_request(Serverurl, Name,RequireGPU, DockerImage, Description, Arguments, OpenPorts)
+Arguments=input("실행에 필요한 인자(Arguments)를 입력하세요.\n입력하지 않으면 인자를 받지 않는 것으로 처리됩니다: ")
+OpenPorts=input("포트를 입력하세요.\n입력하지 않으면 기본포트(12300)으로 처리됩니다: ")
+response = AppRegisterRequest(Serverurl, Name,RequireGPU, DockerImage, Description, Arguments, OpenPorts, ID, PASSWORD)
 
-
-def handle_response(response): # print status
+def handleResponse(response): # print status
     if response.status_code == 200:
         print("앱이 정상적으로 등록되었습니다.")
     elif response.status_code == 400:
@@ -48,4 +55,4 @@ def handle_response(response): # print status
     else:
         print("Unknown Error: 알 수 없는 에러가 발생했습니다.")
 
-handle_response(response)
+handleResponse(response)
