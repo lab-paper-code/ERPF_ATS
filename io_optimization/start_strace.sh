ecd3d10e112c9b74722306ccac8621d1be063c9e
jeun-990806 <kjeun990806@gmail.com>
2023-07-21 17:07:19 +0900

    사용하지 않는 파일 삭제

--- io_optimization/start_strace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-image_num=$1
-batch_size=$2
-epoch=$3
-output_file_path=$4
-
-strace -o ${output_file_path} -r -C -f -e trace=openat,lseek,read,write python3 image_training.py ${image_num} ${batch_size} ${epoch}
